5f343bc60469060ed083c21ba19f2c34a6aff8e2
ashu2120 <160700681+ashu2120@users.noreply.github.com>
2025-04-07 01:19:21 +0530

    Add files via upload

---
+++ Arithmetic.java
@@ -0,0 +1,18 @@
+public class Arithmetic
+{
+    public static void main(String[] args)
+    {
+        int a = 40;
+        int b = 20;
+        int sum = a + b;
+        int difference = a - b;
+        int product = a * b; 
+        int quotient = a / b;
+        int remainder = a % b;
+        System.out.println("The sum of " + a + " and " + b + " is: " + sum);
+        System.out.println("The difference of " + a + " and " + b + " is:: " + difference);
+        System.out.println("The product of " + a + " and " + b + " is: " + product);    
+        System.out.println("The quotient of " + a + " and " + b + " is: " + quotient);
+        System.out.println("The remainder of " + a + " and " + b + " is: " + remainder);
+    }
+}
