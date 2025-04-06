ea6d80f544a30342235550b2585f6f19330f3322
ashu2120 <160700681+ashu2120@users.noreply.github.com>
2025-04-07 01:32:15 +0530

    Add files via upload

---
+++ Largest_num.java
@@ -0,0 +1,10 @@
+public class Largest_num 
+{
+    public static void main(String[] args) 
+    {
+        int a = 10;
+        int b = 20;
+        int largest = (a > b) ? a : b;
+        System.out.println("The largest number is: " + largest);
+    }
+}
