e58bd07334669a20e260f20312c876291dcfdf38
ashu2120 <160700681+ashu2120@users.noreply.github.com>
2025-04-07 01:20:59 +0530

    Add files via upload

---
+++ Compare.java
@@ -0,0 +1,14 @@
+public class Compare {
+    public static void main(String[] args) {
+        int a = 40;
+        int b = 20;
+        if(a>b)
+        {
+            System.out.println("a is greater than b");
+        }
+        else
+        {
+            System.out.println("a is less than b");
+        }
+}
+}
